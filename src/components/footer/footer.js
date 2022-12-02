8eed4e8cf8bf4c6078a75d06597830446fc1f154
Abdullah Shadeer <83266708+Abdulla-Shadeer@users.noreply.github.com>
2022-12-02 17:20:18 +0530

    Add files via upload

---
+++ src/components/footer/footer.js
@@ -0,0 +1,10 @@
+import "./footer.css";
+
+export default function Footer(){
+    return(
+        <div className="footer">
+            <p style={{"fontSize":"18px"}}>© 2022-Fidenz technologies <br/>
+            <span style={{"fontSize":"smaller","color":"skyblue","lineHeight":"1.8"}} >Coded by Abdullah Shadeer</span>  </p>
+        </div>
+    )
+}

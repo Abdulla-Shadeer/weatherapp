ab35ca09927cb59840472de7f84afab6011657ea
Abdullah Shadeer <83266708+Abdulla-Shadeer@users.noreply.github.com>
2022-12-16 14:54:37 +0530

    added table view

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,32 +3,48 @@ import Card from "../card/card.js"
 import cities from "../../cities.json"
 import "./../card/card.css"
 import Footer from "../footer/footer.js"
+import { useState } from "react"
+import Table from "../table/table.js"
+import "./home.css"
 
 export default function Home() {
+    const [option, setOption] = useState("Card")
 
-    setInterval(() => {
-        cities.map(item =>
-            localStorage.removeItem(item.CityCode.toString())
-        )
-    }, 1000 * 60 * 5)
 
     return (
         <>
             <Header />
-            <div className="card-container">
-                {
-                    cities.map((item) => {
-                        return (
-
-                            <Card
-                                key={item.CityCode}
-                                id={item.CityCode}
-                            />
-
-                        )
-                    })
-                }
+            <div className="options">
+                <label htmlFor="option"> Select display format </label>
+                <select name="option" onChange={e => setOption(e.target.value)}>
+                    <option value="Card">
+                        Card
+                    </option>
+                    <option value="Table">
+                        Table
+                    </option>
+                </select>
             </div>
+
+            {option === "Card" ?
+
+                <div className="card-container">
+                    {
+                        cities.map((item) => {
+
+                            return (
+                                <Card
+                                    key={item.CityCode}
+                                    id={item.CityCode}
+                                />
+                            )
+
+                        })
+                    }
+                </div>
+                :
+                    <Table/>
+            }
             <Footer />
         </>
     )

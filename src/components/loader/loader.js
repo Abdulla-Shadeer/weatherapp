bb558757f2933f1991b4e8346b787d1b888df877
Abdullah Shadeer <83266708+Abdulla-Shadeer@users.noreply.github.com>
2022-12-16 14:56:27 +0530

    added table view, loader animation

---
+++ src/components/loader/loader.js
@@ -0,0 +1,8 @@
+import "./loader.css"
+
+export default function Loader(){
+    return(
+        <div className="loader">
+        </div>
+    )
+}
---
+++ src/components/table/tableRow.js
@@ -0,0 +1,33 @@
+import useFetch from "../api/api.js"
+
+export default function TableRow(props) {
+
+    const { data, error } = useFetch(props.id)
+    const imgUrl = process.env.REACT_APP_ICON_URI;
+    let valueArr = []
+
+    if (error) {
+        console.log(error)
+    }
+    else if(data){
+        for (let values in data.list[0].weather[0]){
+        valueArr.push(data.list[0].weather[0][values])
+        }
+    }
+
+    return (
+        data &&
+            <tr>
+                <td>{data.list[0].name}</td>
+                {
+                    valueArr.map((item, i) => {
+                        return (
+                            i === 3 ?
+                                <td key={item}> <img src={imgUrl + item + ".png"} alt={item} /> </td> :
+                                <td key={item}> {item} </td>
+                        )
+                    })
+                }
+            </tr>
+    )
+}
